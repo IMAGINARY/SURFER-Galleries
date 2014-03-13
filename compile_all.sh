8b3a70ee7b6374362f6df6e64b33d5be6a74c882
porst17 <stussak@mfo.de>
2014-03-13 11:41:08 +0100

    removed useless comment

--- compile_all.sh
+++ compile_all.sh
@@ -4,7 +4,6 @@ for GALLERY in "${GALLERIES[@]}"; do
 	cd "$GALLERY"
 	ls -1 -A | grep -v ".DS_Store" | while read DIRNAME
 	do
-	       # Call Function fltyp with the filename as a parameter
 	       cd "${DIRNAME}"
 		latexmk -pdflatex="xelatex --shell-escape -synctex=1" -pdf "${GALLERY}_${DIRNAME}.tex"
 	       cd ..
